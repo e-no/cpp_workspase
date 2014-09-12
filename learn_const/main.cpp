870b90cf2b520309c306ffafdd7592f6c4d1e19c
e-no <b1350009@planet.kanazawa-it.ac.jp>
2014-09-12 19:24:52 +0900

    update

--- learn_const/main.cpp
+++ learn_const/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-
 using namespace std;
+
+namespace Inu{
+    const char* Tiwawa = "tiwawa";
+
+    void Inuname(const char* name){     //犬名表示関数
+        cout << name << endl;
+    }
+
+    void Inuname(){                     //犬名表示関数
+    cout << Tiwawa << endl;
+    }
+}
+
+void Inuname(const char* name){         //グローバルな犬名表示関数
+    cout << name << " " << name << endl;
+}
+
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+
+    Inu::Inuname("sibainu");
+
+    Inuname(Inu::Tiwawa);
+
 }
