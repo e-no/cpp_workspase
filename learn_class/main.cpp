da9b2ce1d4708d4b2a6d4e439e0079a923d49c28
e-no <b1350009@planet.kanazawa-it.ac.jp>
2014-09-17 10:27:25 +0900

    my first class "Inu"

--- learn_class/main.cpp
+++ learn_class/main.cpp
@@ -1,18 +1,40 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 class inu
 {
     private:
-            char    name[20]
-            int     age;
+        char    name[20];
+        int     age;
     public:
-
+        void setname(char* inuname);
+        void setage(int inuage);
+        void display();
 };
+
+void inu::setname(char* inuname)
+{
+    strcpy(name,inuname);
+}
+
+void inu::setage(int inuage)
+{
+    if(inuage<0)    age = 0; else age = inuage;
+}
+
+void inu::display()
+{
+    cout << "–¼‘O:" << name << endl;
+    cout << "”N—î:" <<  age << endl;
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    inu tiwawa;
+
+    tiwawa.setname("ƒ`ƒƒ");
+    tiwawa.setage(10);
+    tiwawa.display();
 }
