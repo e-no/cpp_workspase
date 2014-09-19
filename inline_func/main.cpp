a5d7ef26eb6b326163a5876e317088f332a6835c
e-no <b1350009@planet.kanazawa-it.ac.jp>
2014-09-19 14:42:56 +0900

    update
    inline func

---
+++ inline_func/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+//inline関数について
+
+//inline関数は関数callの高速化の指令であって必ずインライン展開されるわけではない
+
+
+//ケース　１
+//static関数にinlineがついた場合
+static inline int inc(int *a)
+{
+  return (*a)++;
+}
+
+//ケース　２
+//inline なしで宣言された後、 inline 付きで定義された場合
+extern int inc(int *b);
+
+inline int inc(int *b)
+{
+  return (*b)++;
+}
+
+//ケース　３
+// inline付きで定義された場合
+inline int inc(int *c)
+{
+  return (*c)++;
+}
+
+
+//ケース　４
+//extern と inline が同時に指定された場合
+extern inline int inc(int *d)
+{
+  return (*d)++;
+}
+
+
+
+int main()
+{
+
+    cout << "Hello world!" << endl;
+
+    return 0;
+}
---
+++ learn_culculate/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "stdafx.h"
+
+using namespace System;
+
+int main()
+{
+    Console::Title = "ˆê”ÔŠÈ’P‚È”‚Ì•\¦"
+
+    int a = 123 , b = 456;
+
+    Console::WriteLine("A ="+ a +" B = " + b);
+
+    return 0;
+}
